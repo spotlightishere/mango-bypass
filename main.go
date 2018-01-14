d60eddaab3e242472b79ca730e633e75d0b05be9
Spotlight <awesomebing1@gmail.com>
2018-01-14 14:11:32 -0600

    fix user agent, add some sexy code injection

--- main.go
+++ main.go
@@ -3,10 +3,12 @@ package main
 import (
 	"net/http"
 	"log"
-	"fmt"
-	"io"
+
 	"crypto/tls"
 	"github.com/logrusorgru/aurora"
+	"fmt"
+	"io/ioutil"
+	"strings"
 )
 
 var cert tls.Certificate
@@ -35,13 +37,17 @@ func logRequest(handler http.Handler) http.Handler {
 	})
 }
 
-func unknownHandler(w http.ResponseWriter, r *http.Request) {
+func unknownHandler(wiiWriter http.ResponseWriter, wiiRequest *http.Request) {
 	// Proxy from Nintendo.
 
 	// First, add cert to tls handshake...
 	tr := &http.Transport{
 		TLSClientConfig: &tls.Config{
-			Certificates:       []tls.Certificate{cert},
+			Certificates: []tls.Certificate{cert},
+			CipherSuites: []uint16{
+				tls.TLS_RSA_WITH_AES_256_CBC_SHA,
+				tls.TLS_RSA_WITH_RC4_128_SHA,
+			},
 			InsecureSkipVerify: true,
 		},
 	}
@@ -49,18 +55,55 @@ func unknownHandler(w http.ResponseWriter, r *http.Request) {
 		Transport: tr,
 	}
 	// then, mirror current server path...
-	url := fmt.Sprintf("https://oss-auth.shop.wii.com%s", r.URL)
+	url := fmt.Sprintf("https://oss-auth.shop.wii.com%s", wiiRequest.URL)
+	//url := "https://fl0.co/agent.php"
 	log.Print("Mirroring ", aurora.Green(url))
-	response, err := client.Get(url)
+	shopRequest, err := http.NewRequest("GET", url, nil)
 	if err != nil {
 		panic(err)
 	}
-	// and disguise ourselves as the shop channel...
-	response.Header.Add("User-Agent", "Opera/9.30 (Nintendo Wii; U; ; 2077-4; Wii Shop Channel/21.0(A); en)")
 
-	defer response.Body.Close()
+	// Set headers for shop request.
+	// and disguise ourselves as the shop channel, passing on some headers...
+	shopRequest.Header = wiiRequest.Header
+	//shopRequest.Header.Add("Te", "deflate, gzip, chunked, identity, trailers")
+	shopRequest.Header.Add("Accept", "text/html, application/xml;q=0.9, application/xhtml+xml, image/png, image/jpeg, image/gif, image/x-xbitmap, */*;q=0.1")
+	shopRequest.Header.Add("Accept-Language", "en")
+	shopRequest.Header.Add("Accept-Charset", "iso-8859-1, utf-8, utf-16, *;q=0.1")
+	//shopRequest.Header.Add("Accept-Encoding", "deflate, gzip, x-gzip, identity, *;q=0")
+	//shopRequest.Header.Add("Connection", "Keep-Alive, TE")
+	shopRequest.Header.Add("Connection", "Keep-Alive")
+	shopRequest.Header.Set("User-Agent", "Opera/9.30 (Nintendo Wii; U; ; 2077-4; Wii Shop Channel/21.0(A); en)")
+
+	shopResponse, err := client.Do(shopRequest)
+	if err != nil {
+		panic(err)
+	}
+
+	defer shopResponse.Body.Close()
+	shopBody, err := ioutil.ReadAll(shopResponse.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	// Check if we need to mirror directly or not.
+	if strings.Contains(wiiRequest.URL.Path, "jsp") {
+		// grab our cool api
+		api, err := ioutil.ReadFile("./cool-api.js")
+		if err != nil {
+			panic(err)
+		}
+
+		shopBody = append(shopBody, []byte("<script>\n")...)
+		shopBody = append(shopBody, api...)
+		shopBody = append(shopBody, []byte("\n</script>")...)
+
+	} else {
+		// don't need to inject
+	}
+
 	// then, literally mirror back!
-	_, err = io.Copy(w, response.Body)
+	_, err = wiiWriter.Write(shopBody)
 	if err != nil {
 		panic(err)
 	}
