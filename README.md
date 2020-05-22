ff7916822baa26b45c946f674f9c33b5137e6a3e
Spotlight <spotlight@joscomputing.space>
2020-05-21 20:44:43 -0500

    Append textarea for ec log

--- README.md
+++ README.md
@@ -1,19 +1,18 @@
-# the heck is this
-good question lol
+# What's this?
+It connects to the Wii Shop Channel for you, and proxies GET/POST requests. This allows you to modify what's passed through to a server you control.
 
-# so what do???
-if you really wanna hax0r that ninty server, here's a cool how2:
-1. go to https://larsenv.github.io/NintendoCerts/index.html
-2. download "Wii NWC Prod 1"
-3. copypasta wherever you'd like, i put it in this repo (since ignored)
-4.
+# How do I set it up?
+1. Do to https://larsenv.github.io/NintendoCerts/index.html
+2. Download "Wii NWC Prod 1" wherever you'd like, i put it in this repo (since ignored)
+3.
 ```
 openssl pkcs12 -in WII_NWC_1_CERT.p12 -out shop.crt -clcerts -nokeys
 openssl pkcs12 -in WII_NWC_1_CERT.p12 -out shop.key -nocerts -nodes
 ```
-5. use magic, edit wii shop dol to have `https://oss-auth.shop.wii.com/` -> `http://oss-auth.shop.wii.com/`
-6. using similar magic, edit `00000002.app`'s U8 and edit the opera include file to have `http://*.shop.wii.com`
-7. grab yourself a dns server, bind `oss-auth.shop.wii.com` to wherever you wanna run this
+4. use magic, edit wii shop dol to have `https://oss-auth.shop.wii.com/` -> `http://oss-auth.shop.wii.com/`
+5. using similar magic, edit `00000002.app`'s U8 and edit the opera include file to have `http://*.shop.wii.com`
+6. grab yourself a dns server, bind `oss-auth.shop.wii.com` to wherever you wanna run this
+7. copy `config.go.example` to `config.co`, using similar as above (`oss-auth.shop.wii.com`) if you're editing DNS, or if you've changed CAs your own domain
 8. fire this up
 9. ???
 10. potentially quite literally, profit
--- main.go
+++ main.go
@@ -1,17 +1,14 @@
 package main
 
 import (
-	"net/http"
+	"bytes"
 	"log"
+	"net/http"
 
 	"crypto/tls"
-	"github.com/logrusorgru/aurora"
 	"fmt"
+	"github.com/logrusorgru/aurora"
 	"io/ioutil"
-	//"strings"
-	"path"
-	"os"
-	"strings"
 )
 
 var cert tls.Certificate
@@ -25,47 +22,24 @@ func main() {
 	cert = shopCert
 
 	http.HandleFunc("/", unknownHandler)
+	http.HandleFunc("/verysecrethandler", secretHandler)
 	log.Println("Running...")
-	http.ListenAndServe(":8888", logRequest(http.DefaultServeMux))
+	http.ListenAndServe("192.168.3.135:8080", logRequest(http.DefaultServeMux))
 }
 
 func logRequest(handler http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		log.Printf("%s %s", aurora.Blue(r.Method), aurora.Red(r.URL))
-		//// TODO: remove header dumping
+		// TODO: remove header dumping
 		//for name, value := range r.Header {
 		//	log.Print(name, " ", aurora.Green("=>"), " ", value)
 		//}
-		log.Printf("Accessing from: %s", aurora.Blue(r.Host))
+		//log.Printf("Accessing from: %s", aurora.Blue(r.Host))
 		handler.ServeHTTP(w, r)
 	})
 }
 
 func unknownHandler(wiiWriter http.ResponseWriter, wiiRequest *http.Request) {
-	// First, we need to check if stuff has been "cached".
-	filename := path.Base(wiiRequest.URL.Path)
-	if filename == "/" {
-		filename = "index.html"
-	}
-	firstLayout := strings.Replace(wiiRequest.URL.Path, "/oss/oss/", "", -1)
-	layout := strings.Replace(firstLayout, "/"+filename, "", -1)
-	os.MkdirAll("cache/"+layout, os.ModePerm)
-
-	cachedPath := "cache/" + layout + "/" + filename
-	if _, err := os.Stat(cachedPath); os.IsNotExist(err) {
-		// not cached! we'll get 'er next time
-		log.Printf("Looks like %s isn't cached, requesting.", aurora.Blue(filename))
-	} else {
-		// ok we're good lol
-		coolFile, err := ioutil.ReadFile(cachedPath)
-		if err != nil {
-			panic(err)
-		}
-
-		wiiWriter.Write(coolFile)
-		return
-	}
-
 	// Not cached, so "proxy" from Nintendo.
 
 	// First, add cert to tls handshake...
@@ -82,25 +56,26 @@ func unknownHandler(wiiWriter http.ResponseWriter, wiiRequest *http.Request) {
 	client := &http.Client{
 		Transport: tr,
 	}
-	// then, mirror current server path...
-	url := fmt.Sprintf("https://oss-auth.shop.wii.com%s", wiiRequest.URL)
-	//url := "https://fl0.co/agent.php"
+
+	url := fmt.Sprintf("https://%s%s", ProxyingDomain, wiiRequest.URL)
 	log.Print("Mirroring ", aurora.Green(url))
-	shopRequest, err := http.NewRequest("GET", url, nil)
-	if err != nil {
-		panic(err)
+
+	var shopRequest *http.Request
+	var err error
+
+	// then, mirror current server path...
+	if wiiRequest.Method == "GET" {
+		shopRequest, err = http.NewRequest("GET", url, nil)
+		if err != nil {
+			panic(err)
+		}
+	} else if wiiRequest.Method == "POST" {
+		shopRequest, err = http.NewRequest("POST", url, wiiRequest.Body)
 	}
 
 	// Set headers for shop request.
 	// and disguise ourselves as the shop channel, passing on some headers...
 	shopRequest.Header = wiiRequest.Header
-	//shopRequest.Header.Add("Te", "deflate, gzip, chunked, identity, trailers")
-	//shopRequest.Header.Add("Accept", "text/html, application/xml;q=0.9, application/xhtml+xml, image/png, image/jpeg, image/gif, image/x-xbitmap, */*;q=0.1")
-	//shopRequest.Header.Add("Accept-Language", "en")
-	//shopRequest.Header.Add("Accept-Charset", "iso-8859-1, utf-8, utf-16, *;q=0.1")
-	//shopRequest.Header.Add("Accept-Encoding", "deflate, gzip, x-gzip, identity, *;q=0")
-	//shopRequest.Header.Add("Connection", "Keep-Alive, TE")
-	////shopRequest.Header.Add("Connection", "Keep-Alive")
 	shopRequest.Header.Set("User-Agent", "Opera/9.30 (Nintendo Wii; U; ; 2077-4; Wii Shop Channel/21.0(A); en)")
 
 	shopResponse, err := client.Do(shopRequest)
@@ -114,10 +89,21 @@ func unknownHandler(wiiWriter http.ResponseWriter, wiiRequest *http.Request) {
 		panic(err)
 	}
 
-	// Cache them, by all means!
-	err = ioutil.WriteFile(cachedPath, shopBody, os.ModePerm)
-	if err != nil {
-		panic(err)
+	if bytes.Contains(shopBody, []byte(ProxyingDomain)) {
+		shopBody = bytes.ReplaceAll(shopBody, []byte(ProxyingDomain), []byte(ConnectingDomain))
+	}
+
+	if bytes.Contains(shopBody, []byte("</body>")) {
+		shopBody = bytes.ReplaceAll(shopBody, []byte("</body>"), []byte(`
+	<script>
+		var elem = document.createElement('textarea');
+		elem.innerHTML = ec.getLog();
+		elem.setAttribute("rows", "15");
+		elem.setAttribute("cols", "75");
+		document.body.appendChild(elem);
+	</script>
+</body>
+`))
 	}
 
 	// then, literally mirror back!
@@ -125,4 +111,8 @@ func unknownHandler(wiiWriter http.ResponseWriter, wiiRequest *http.Request) {
 	if err != nil {
 		panic(err)
 	}
+}
+
+func secretHandler(wiiWriter http.ResponseWriter, wiiRequest *http.Request) {
+
 }
