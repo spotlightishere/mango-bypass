68a78aba87fc8fb4595b1309f1a820f0699ca363
Spotlight <spotlight@joscomputing.space>
2020-05-22 07:38:00 -0500

    Properly proxy server headers

--- main.go
+++ main.go
@@ -31,12 +31,11 @@ func main() {
 
 func logRequest(handler http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		log.Printf("%s %s", aurora.Blue(r.Method), aurora.Red(r.URL))
+		log.Printf("%s %s via %s", aurora.Blue(r.Method), aurora.Red(r.URL), aurora.Cyan(r.Host))
 		// TODO: remove header dumping
 		//for name, value := range r.Header {
 		//	log.Print(name, " ", aurora.Green("=>"), " ", value)
 		//}
-		log.Printf("Accessing from: %s", aurora.Cyan(r.Host))
 		handler.ServeHTTP(w, r)
 	})
 }
@@ -63,7 +62,8 @@ func unknownHandler(wiiWriter http.ResponseWriter, wiiRequest *http.Request) {
 	// If we've done our job right, however, inbound requests will already be to our proxy, so we need to rewrite.
 	properDomain := strings.ReplaceAll(wiiRequest.Host, ProxyingDomain, OfficialDomain)
 
-	url := fmt.Sprintf("https://%s%s", properDomain, wiiRequest.URL)
+	properPath := wiiRequest.URL.String()
+	url := fmt.Sprintf("https://%s%s", properDomain, properPath)
 	log.Print("Proxying from ", aurora.Green(url))
 
 	var shopRequest *http.Request
@@ -71,7 +71,8 @@ func unknownHandler(wiiWriter http.ResponseWriter, wiiRequest *http.Request) {
 
 	shouldLog := false
 	// If this is a java servlet, we would definitely like to know :)
-	if strings.Contains(wiiRequest.URL.Path, ".jsp") {
+	// Unless you're the same-old check-in pages. Then we would not.
+	if strings.Contains(wiiRequest.URL.Path, ".jsp") && !strings.Contains(wiiRequest.URL.Path, "Check") {
 		shouldLog = true
 	}
 
@@ -102,6 +103,10 @@ func unknownHandler(wiiWriter http.ResponseWriter, wiiRequest *http.Request) {
 	shopRequest.Header = wiiRequest.Header
 	shopRequest.Header.Set("User-Agent", "Opera/9.30 (Nintendo Wii; U; ; 2077-4; Wii Shop Channel/21.0(A); en)")
 
+	// Cache somehow messes up proxying. TODO: Investigate - is it because of a change applied?
+	shopRequest.Header.Del("If-Modified-Since")
+	shopRequest.Header.Del("If-None-Match")
+
 	shopResponse, err := client.Do(shopRequest)
 	if err != nil {
 		panic(err)
@@ -118,33 +123,43 @@ func unknownHandler(wiiWriter http.ResponseWriter, wiiRequest *http.Request) {
 
 	// Make life a little easier for "progress object" errors.
 	shopBody = bytes.ReplaceAll(shopBody, []byte("var errInfo = progress.errInfo;"), []byte(`
-var errInfo = progress.errInfo;
-trace("status: " + progress.status);
-trace("error code: " + progress.errCode);
-trace("error info: " + progress.errInfo);
-`))
+	var errInfo = progress.errInfo;
+	trace("status: " + progress.status);
+	trace("error code: " + progress.errCode);
+	trace("error info: " + progress.errInfo);
+	`))
 
 	// And inject our text area in as well.
 	shopBody = bytes.ReplaceAll(shopBody, []byte("</body>"), []byte(`
-	<script>
-		var a = document.createElement('a');
-		a.setAttribute("href", "/eval.html");
-		var button = document.createElement('button');
-		button.setAttribute("type", "button");
-		button.innerHTML = "Eval";
-		a.appendChild(button);
-		document.body.appendChild(a);
-	</script>
-</body>
-`))
+		<script>
+			var a = document.createElement('a');
+			a.setAttribute("href", "/eval.html");
+			var button = document.createElement('button');
+			button.setAttribute("type", "button");
+			button.innerHTML = "Eval";
+			a.appendChild(button);
+			document.body.appendChild(a);
+		</script>
+	</body>
+	`))
 
 	if shouldLog {
 		log.Println(string(shopBody))
 	}
 
 	// then, literally mirror back!
+	// + headers too!
+	for name, values := range shopResponse.Header {
+		if name == "Content-Length" {
+			continue
+		}
+		for _, value := range values {
+			wiiWriter.Header().Add(name, value)
+		}
+	}
 	_, err = wiiWriter.Write(shopBody)
 	if err != nil {
 		panic(err)
 	}
+	log.Printf("Returned proxied error code %d", shopResponse.StatusCode)
 }
